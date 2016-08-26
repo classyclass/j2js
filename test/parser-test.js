b6e5fb1f15bf01388273f820457cb1fe774f7b50
Kyumin Kim <math4tots@gmail.com>
2016-08-26 02:19:56 -0700

    parser 3.2: more parser WIP

--- test/parser-test.js
+++ test/parser-test.js
@@ -1,6 +1,7 @@
 // lexer-test.js
 const chai = require("chai");
 const parser = require("../lib/parser.js");
+const ast = require("../lib/ast.js");
 const expect = chai.expect;
 
 describe("parser", () => {
@@ -54,6 +55,56 @@ describe("parser", () => {
       expect(method.returnType).to.equal("void");
       expect(method.args.length).to.equal(0);
     }
+  });
+  it("parse an add expression", () => {
+    const expr = parser.parseExpression("5 + 6");
+    expect(expr).to.be.an.instanceof(ast.Operator);
+    expect(expr.operator).to.equal("+");
+    expect(expr.args.length).to.equal(2);
+    expect(expr.args[0]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[0].value).to.equal(5);
+    expect(expr.args[1]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[1].value).to.equal(6);
+  });
+  it("parse a post-increment expression", () => {
+    const expr = parser.parseExpression("x++");
+    expect(expr).to.be.an.instanceof(ast.Operator);
+    expect(expr.operator).to.equal("++");
+    expect(expr.args.length).to.equal(1);
+    expect(expr.args[0]).to.be.an.instanceof(ast.Name);
+    expect(expr.args[0].name).to.equal("x");
+  });
+  it("parse a ternary expression", () => {
+    const expr = parser.parseExpression("1 ? 2 : 3");
+    expect(expr).to.be.an.instanceof(ast.Operator);
+    expect(expr.operator).to.equal("?:");
+    expect(expr.args.length).to.equal(3);
+    expect(expr.args[0]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[0].value).to.equal(1);
+    expect(expr.args[1]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[1].value).to.equal(2);
+    expect(expr.args[2]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[2].value).to.equal(3);
+  });
+  it("parse an assign expression", () => {
+    const expr = parser.parseExpression("x = 1");
+    expect(expr).to.be.an.instanceof(ast.Operator);
+    expect(expr.operator).to.equal("=");
+    expect(expr.args.length).to.equal(2);
+    expect(expr.args[0]).to.be.an.instanceof(ast.Name);
+    expect(expr.args[0].name).to.equal("x");
+    expect(expr.args[1]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[1].value).to.equal(1);
+  });
+  it("parse an field assign expression", () => {
+    const expr = parser.parseExpression("a.x = 1");
+    expect(expr).to.be.an.instanceof(ast.Operator);
+    expect(expr.operator).to.equal("=");
+    expect(expr.args.length).to.equal(2);
+    expect(expr.args[0]).to.be.an.instanceof(ast.GetField);
+    expect(expr.args[0].name).to.equal("x");
+    expect(expr.args[1]).to.be.an.instanceof(ast.Int);
+    expect(expr.args[1].value).to.equal(1);
   });
 });
 
